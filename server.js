bbe57797f0fcdc6884b454d331bb4e5aa8939d1b
XxJefinhooxX <jefersonluis@live.com>
2025-04-16 21:36:26 -0400

    Atualiza URL do Ngrok e ajustes no servidor

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const TELEGRAM_BOT_TOKEN = "BOT-TOKEN"; // Substitua pelo token do seu bot
-const TELEGRAM_CHAT_ID = "CHAT-TOKEN"; // Substitua pelo ID do chat (ou grupo) para onde quer enviar
+const TELEGRAM_BOT_TOKEN = "7754807952:AAEQUfVhxGsbbQ2Q-vpvESPOTIrluWOv7dU"; // Substitua pelo token do seu bot
+const TELEGRAM_CHAT_ID = "2095674031"; // Substitua pelo ID do chat (ou grupo) para onde quer enviar
 
 app.post("/send-location", async (req, res) => {
   const { latitude, longitude, maps } = req.body;
